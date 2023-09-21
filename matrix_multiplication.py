edd6fba77e94ba94ae1ea81dd3405b7c30237daa
uarif <uarif@users.noreply.github.com>
2023-09-21 17:18:24 -0400

    matrix_multiplication.py

--- matrix_multiplication.py
+++ matrix_multiplication.py
@@ -1,1 +1,6 @@
-
+#Importing some necessary liberaries for the code.
+import numpy as np
+import time
+# Create random matrices of 100*100 dimentions. 
+A = np.random.rand(100, 100)
+B = np.random.rand(100, 100)
