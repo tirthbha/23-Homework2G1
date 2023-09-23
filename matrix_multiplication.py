64920e29dcf4380eb2b967c024393ce3f9077cb3
uarif <uarif@users.noreply.github.com>
2023-09-23 17:27:50 -0400

    Update matrix_multiplication.py
    Matrices defined with dimentions

--- matrix_multiplication.py
+++ matrix_multiplication.py
@@ -1,9 +1,9 @@
 #Importing some necessary liberaries for the code.
 import numpy as np
 import time
-# Create random matrices of 100*100 dimentions. 
-A = np.random.rand(100, 100)
-B = np.random.rand(100, 100)
+# Creating two matrices A, B of dimention 3*3. 
+A = np.array([[200,533,233],[532,332,534],[6342,4434,3434]])
+B = np.array([[4454,5545,5456],[2423,434,345],[6434,84,344]])
 # Method 1: Manual matrix multiplication using nested loops
 def matrix_multiply_manual(A, B):
     C = np.zeros((A.shape[0], B.shape[1]))
